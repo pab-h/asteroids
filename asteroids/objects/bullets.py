f828e051006c1f645d6c96aa7581357e11d9a7db
pab-h <dev.pab.2020@gmail.com>
2024-02-07 18:24:32 -0300

    feat: kill e iter para a classe de grupo das balas

--- asteroids/objects/bullets.py
+++ asteroids/objects/bullets.py
@@ -3,14 +3,22 @@ from pygame import Surface
 from asteroids.interfaces.updateable import Updateable
 from asteroids.objects.bullet import Bullet
 
+from typing import Iterator
+
 class Bullets(Updateable):
     def __init__(self) -> None:
         super().__init__()
 
         self.bullets: list[Bullet] = []
 
+    def __iter__(self) -> Iterator[Bullet]:
+        return iter(self.bullets)
+
     def addAll(self, bullets: list[Bullet]) -> None:
         self.bullets = self.bullets + bullets
+
+    def kill(self, bullet: Bullet) -> None:
+        self.bullets.remove(bullet)
 
     def update(self, screen: Surface, dt: float) -> None:
         for bullet in self.bullets:
