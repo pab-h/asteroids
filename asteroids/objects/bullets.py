a50f9197374d71e32a02809b73dc3808878ad3b3
pab-h <dev.pab.2020@gmail.com>
2024-02-19 09:59:35 -0300

    feat: garbage collector bullets

--- asteroids/objects/bullets.py
+++ asteroids/objects/bullets.py
@@ -1,4 +1,7 @@
+import pygame.display as display
+
 from pygame import Surface
+from pygame import Vector2
 
 from asteroids.interfaces.updateable import Updateable
 from asteroids.objects.bullet import Bullet
@@ -10,6 +13,8 @@ class Bullets(Updateable):
         super().__init__()
 
         self.bullets: list[Bullet] = []
+        self.center = self.getScreenCenter()
+        self.cutRadius = self.center.length()
 
     def __iter__(self) -> Iterator[Bullet]:
         return iter(self.bullets)
@@ -20,6 +25,21 @@ class Bullets(Updateable):
     def kill(self, bullet: Bullet) -> None:
         self.bullets.remove(bullet)
 
+    def getScreenCenter(self) -> Vector2:
+        size = display.get_window_size()
+
+        return Vector2(size) / 2
+
+    def garbage(self) -> None:
+        for bullet in self.bullets:
+            radiusVector = self.center - bullet.position
+            radius = radiusVector.length()
+            
+            if radius > self.cutRadius:
+                self.kill(bullet)
+
     def update(self, screen: Surface, dt: float) -> None:
+        self.garbage()
+
         for bullet in self.bullets:
             bullet.update(screen, dt)
